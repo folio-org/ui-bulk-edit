1ff2325d804632711bd5e4c32c826b5217ece5e0
uladzislau_kutarkin <uladzislau_kutarkin@epam.com>
2021-10-20 11:36:36 +0300

    UIBULKED-5: Add Apps with bulk edit capability accordion

--- src/BulkEditList/BulkEditListFilters/BulkEditListFilters.js
+++ src/BulkEditList/BulkEditListFilters/BulkEditListFilters.js
@@ -5,14 +5,22 @@ import {
   Button,
   ButtonGroup,
 } from '@folio/stripes/components';
+import { AcqCheckboxFilter } from '@folio/stripes-acq-components';
 
 import { ListSelect } from './ListSelect/ListSelect';
 import { ListFileUploader } from './ListFileUploader/ListFileUploader';
+import { buildCheckboxFilterOptions } from './utils';
+import { EDIT_CAPABILITIES } from '../../constants/optionsRecordIdentifiers';
 
 export const BulkEditListFilters = () => {
   const [criteria, setCriteria] = useState('identifier');
   const [isLoading, setLoading] = useState(false);
   const [isDropZoneActive, setDropZoneActive] = useState(false);
+  const [filters, setFilter] = useState({
+    capabilities: ['users'],
+  });
+
+  const capabilitiesFilterOptions = buildCheckboxFilterOptions(EDIT_CAPABILITIES);
 
   const renderIdentifierButton = () => {
     return (
@@ -62,6 +70,16 @@ export const BulkEditListFilters = () => {
         handleDragEnter={handleDragEnter}
         handleDrop={handleDrop}
         handleDragLeave={handleDragLeave}
+      />
+      <AcqCheckboxFilter
+        labelId="ui-bulk-edit.list.filters.capabilities.title"
+        options={capabilitiesFilterOptions}
+        name="capabilities"
+        activeFilters={filters.capabilities}
+        onChange={(event) => setFilter({
+          ...filters, capabilities: event.values,
+        })}
+        closedByDefault={false}
       />
     </>
   );
