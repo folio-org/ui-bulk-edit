58938488bceb37eaa834ca7c97ab1811e602ad08
uladzislau_kutarkin <uladzislau_kutarkin@epam.com>
2021-11-28 16:05:31 +0300

    rebase on master

--- src/components/BulkEditList/BulkEditListFilters/ListFileUploader/ListFileUploader.test.js
+++ src/components/BulkEditList/BulkEditListFilters/ListFileUploader/ListFileUploader.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {
-  screen, render, fireEvent,
+  screen,
+  render,
+  fireEvent,
 } from '@testing-library/react';
 
 import '../../../../../test/jest/__mock__';
