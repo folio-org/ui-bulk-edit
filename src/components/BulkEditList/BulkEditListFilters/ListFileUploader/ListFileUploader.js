498d049f43e24f1e8120519e11c518fba5fd8137
Vlad Samets <uladislau_samets@epam.com>
2021-11-30 11:14:25 +0300

    UIBULKED-24 Link record identifier with the drag and drop area on the landing page

--- src/components/BulkEditList/BulkEditListFilters/ListFileUploader/ListFileUploader.js
+++ src/components/BulkEditList/BulkEditListFilters/ListFileUploader/ListFileUploader.js
@@ -8,16 +8,28 @@ import {
 } from '@folio/stripes-data-transfer-components';
 
 export const ListFileUploader = (
-  { isDropZoneActive,
+  {
+    isDropZoneActive,
     isLoading,
     handleDragEnter,
     handleDragLeave,
-    handleDrop },
+    handleDrop,
+    selectedIdentifier,
+  },
 ) => {
   const uploaderTitle = isDropZoneActive ? isLoading
     ? <Preloader message={<FormattedMessage id="ui-bulk-edit.uploading" />} />
     : <FormattedMessage id="ui-bulk-edit.uploaderActiveTitle" />
     : <FormattedMessage id="ui-bulk-edit.uploaderTitle" />;
+
+  const usploderSubTitle = (
+    <FormattedMessage
+      id="ui-bulk-edit.uploaderSubTitle"
+      values={{
+        identifier: selectedIdentifier,
+      }}
+    />
+  );
 
   return (
     <div style={{ marginBottom: '15px' }}>
@@ -26,6 +38,7 @@ export const ListFileUploader = (
         title={uploaderTitle}
         uploadButtonText={<FormattedMessage id="ui-bulk-edit.uploaderBtnText" />}
         isDropZoneActive={isDropZoneActive}
+        disabled={!selectedIdentifier}
         onDragEnter={handleDragEnter}
         onDragLeave={handleDragLeave}
         onDrop={handleDrop}
@@ -34,7 +47,7 @@ export const ListFileUploader = (
         <>
           <Layout className="padding-top-gutter padding-start-gutter padding-end-gutter textCentered">
             <span data-test-sub-title>
-              <FormattedMessage id="ui-bulk-edit.uploaderSubTitle" />
+              {usploderSubTitle}
             </span>
           </Layout>
         </>
@@ -50,4 +63,5 @@ ListFileUploader.propTypes = {
   handleDragEnter: PropTypes.func.isRequired,
   handleDragLeave: PropTypes.func.isRequired,
   handleDrop: PropTypes.func.isRequired,
+  selectedIdentifier: PropTypes.string,
 };
