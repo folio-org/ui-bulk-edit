e35e5f88f03b81af9525cb36adcea55061b6679e
vashjs <86330150+vashjs@users.noreply.github.com>
2022-01-18 16:07:38 +0200

    UIBULKED-22 Completion of the bulk edit (#42)

--- src/components/BulkEditList/BulkEditListResult/Preview/Preview.js
+++ src/components/BulkEditList/BulkEditListResult/Preview/Preview.js
@@ -1,15 +1,22 @@
 import PropTypes from 'prop-types';
 import { Headline, MessageBanner } from '@folio/stripes/components';
 import { FormattedMessage } from 'react-intl';
+import { useParams } from 'react-router';
+import { useDownloadLinks } from '../../../../API/useDownloadLinks';
 
-export const Preview = ({ fileUploadedName, processedRecords }) => {
+export const Preview = ({ fileUploadedName }) => {
+  const { id } = useParams();
+  const { data } = useDownloadLinks(id);
+
+  const processed = data?.progress?.processed;
+
   return (
     <>
-      {processedRecords && (
+      {processed && (
       <MessageBanner type="success" contentClassName="SuccessBanner">
         <FormattedMessage
           id="ui-bulk-edit.recordsSuccessfullyChanged"
-          values={{ value: processedRecords }}
+          values={{ value: processed }}
         />
       </MessageBanner>)
       }
@@ -24,5 +31,4 @@ export const Preview = ({ fileUploadedName, processedRecords }) => {
 
 Preview.propTypes = {
   fileUploadedName: PropTypes.string,
-  processedRecords: PropTypes.number,
 };
--- src/components/BulkEditList/BulkEditListResult/Preview/Preview.test.js
+++ src/components/BulkEditList/BulkEditListResult/Preview/Preview.test.js
@@ -1,16 +1,39 @@
 import { render, screen } from '@testing-library/react';
+import { useOkapiKy } from '@folio/stripes/core';
 
 import '../../../../../test/jest/__mock__';
 
+import { MemoryRouter } from 'react-router';
+import { QueryClientProvider } from 'react-query';
 import { Preview } from './Preview';
+import { queryClient } from '../../../../../test/jest/utils/queryClient';
 
 const renderPreview = () => {
   render(
-    <Preview fileUploadedName="Mock.csv" />,
+    <MemoryRouter initialEntries={['/bulk-edit/1']}>
+      <QueryClientProvider client={queryClient}>
+        <Preview fileUploadedName="Mock.csv" />,
+      </QueryClientProvider>
+    </MemoryRouter>,
   );
 };
 
 describe('Preview', () => {
+  beforeEach(() => {
+    useOkapiKy
+      .mockClear()
+      .mockReturnValue({
+        get: () => ({
+          json: () => ({
+            files: ['success.csv', 'error.csv'],
+            progress: {
+              progress: 55,
+            },
+          }),
+        }),
+      });
+  });
+
   it('displays Bulk edit', () => {
     renderPreview();
 
