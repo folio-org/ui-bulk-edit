c77c71b045b1f25937fb6c0dc84e2ff4ccb6b6c5
uladzislau_kutarkin <uladzislau_kutarkin@epam.com>
2022-03-18 11:01:34 +0300

    fix comments

--- src/components/BulkEditList/BulkEditListFilters/ListSelect/ListSelect.js
+++ src/components/BulkEditList/BulkEditListFilters/ListSelect/ListSelect.js
@@ -11,16 +11,16 @@ export const ListSelect = ({ disabled, onChange, capabilities }) => {
   const intl = useIntl();
   const location = useLocation();
 
-  let option;
+  let defaultOption;
 
-  const getOption = (recordType) => {
-    const options = identifierOptions[recordType].map((el) => ({
+  const getOption = () => {
+    const options = identifierOptions[capabilities].map((el) => ({
       value: el.value,
       label: intl.formatMessage({ id: el.label }),
       disabled: el.disabled,
     }));
 
-    option = options[0].value;
+    defaultOption = options[0].value;
 
     return options;
   };
@@ -28,13 +28,13 @@ export const ListSelect = ({ disabled, onChange, capabilities }) => {
   const defaultIdentifier = useMemo(() => {
     const identifier = new URLSearchParams(location.search).get('identifier');
 
-    return identifier || option;
+    return identifier || defaultOption;
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location.search]);
 
   return (
     <Select
-      dataOptions={getOption(capabilities)}
+      dataOptions={getOption()}
       arial-label={intl.formatMessage({ id: 'ui-bulk-edit.list.filters.recordIdentifier' })}
       label={<FormattedMessage id="ui-bulk-edit.list.filters.recordIdentifier" />}
       defaultValue={defaultIdentifier}
