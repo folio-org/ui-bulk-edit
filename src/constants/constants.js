8103aa3b1b37957c8c219789e8845b01d651792a
uladzislau_kutarkin <uladzislau_kutarkin@epam.com>
2022-05-19 13:05:53 +0400

    UIBULKED-38: move constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -231,6 +231,59 @@ export const ITEMS_OPTIONS = [
   },
 ];
 
+export const ITEM_STATUS_OPTIONS = [
+  {
+    value: '',
+    label: 'ui-bulk-edit.layer.options.placeholder',
+    disabled: true,
+  },
+  {
+    value: 'Available',
+    label: 'ui-bulk-edit.layer.options.available',
+    disabled: false,
+  },
+  {
+    value: 'Missing',
+    label: 'ui-bulk-edit.layer.options.missing',
+    disabled: false,
+  },
+  {
+    value: 'Withdrawn',
+    label: 'ui-bulk-edit.layer.options.withdrawn',
+    disabled: false,
+  },
+  {
+    value: 'In process (non-requestable)',
+    label: 'ui-bulk-edit.layer.options.non-requestable',
+    disabled: false,
+  },
+  {
+    value: 'Intellectual item"',
+    label: 'ui-bulk-edit.layer.options.intellectual',
+    disabled: false,
+  },
+  {
+    value: 'Long missing',
+    label: 'ui-bulk-edit.layer.options.longMissing',
+    disabled: false,
+  },
+  {
+    value: 'Restricted',
+    label: 'ui-bulk-edit.layer.options.restricted',
+    disabled: false,
+  },
+  {
+    value: 'Unavailable',
+    label: 'ui-bulk-edit.layer.options.unavailable',
+    disabled: false,
+  },
+  {
+    value: 'Unknown',
+    label: 'ui-bulk-edit.layer.options.unknown',
+    disabled: false,
+  },
+];
+
 export const TYPE_OF_PROGRESS = {
   INITIAL: 'initial',
   PROCESSED: 'processed',
