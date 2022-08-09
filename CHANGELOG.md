5d44c675d6eb7fcbfbb227b5ffb7682631d73865
uladzislau_kutarkin <uladzislau_kutarkin@epam.com>
2022-08-09 11:10:40 +0400

    UIBULKED-140: Update ITEMS content update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 ## In progress
 
 * [UIBULKED-127](https://issues.folio.org/browse/UIBULKED-127) Type non existing location and press enter. Error while bulk edit items.
+* [UIBULKED-140](https://issues.folio.org/browse/UIBULKED-140) Update ITEMS content update
 
 ## [2.0.2](https://github.com/folio-org/ui-bulk-edit/tree/v2.0.2) (2022-08-05)
 * [UIBULKED-130](https://issues.folio.org/browse/UIBULKED-130) Progress bar blinks (reloads) on the first run
--- src/API/useInAppUpload.js
+++ src/API/useInAppUpload.js
@@ -1,16 +1,14 @@
 import { useMutation } from 'react-query';
 
 import { useOkapiKy } from '@folio/stripes/core';
-import { CAPABILITIES_PARAMS } from '../constants';
 
 export const useInAppUpload = () => {
   const ky = useOkapiKy();
 
   const { mutateAsync: inAppUpload, isLoading } = useMutation({ mutationFn: ({ jobId, contentUpdates }) => {
-    return ky.post(`bulk-edit/${jobId}/items-content-update/upload`, {
+    return ky.post(`bulk-edit/${jobId}/item-content-update/upload`, {
       searchParams: { limit: 10 },
       json: {
-        entityType: CAPABILITIES_PARAMS.ITEM,
         contentUpdates,
         totalRecords: contentUpdates.length,
       },
