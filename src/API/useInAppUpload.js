adbcbc26526307ab791047dd2d2e681d90d584b1
UladzislauKutarkin <72550466+UladzislauKutarkin@users.noreply.github.com>
2022-08-10 14:20:23 +0400

    UIBULKED-140: Update ITEMS content update (#158)

--- src/API/useInAppUpload.js
+++ src/API/useInAppUpload.js
@@ -1,17 +1,15 @@
 import { useMutation } from 'react-query';
 
 import { useOkapiKy } from '@folio/stripes/core';
-import { CAPABILITIES_PARAMS } from '../constants';
 
 export const useInAppUpload = () => {
   const ky = useOkapiKy();
 
   const { mutateAsync: inAppUpload, isLoading } = useMutation({ mutationFn: ({ jobId, contentUpdates }) => {
-    return ky.post(`bulk-edit/${jobId}/items-content-update/upload`, {
+    return ky.post(`bulk-edit/${jobId}/item-content-update/upload`, {
       searchParams: { limit: 10 },
       json: {
-        entityType: CAPABILITIES_PARAMS.ITEM,
-        contentUpdates,
+        itemContentUpdates: contentUpdates,
         totalRecords: contentUpdates.length,
       },
     }).json();
