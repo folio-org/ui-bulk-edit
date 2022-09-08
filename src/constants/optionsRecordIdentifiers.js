744f92f8fa4397cce01b616ebfd11aaad03c53f3
vashjs <86330150+vashjs@users.noreply.github.com>
2022-09-08 11:15:51 +0300

    UIBULKED-110 Populate holdings record identifiers dropdown (#172)

--- src/constants/optionsRecordIdentifiers.js
+++ src/constants/optionsRecordIdentifiers.js
@@ -54,6 +54,28 @@ export const identifierOptions = {
       label: 'ui-bulk-edit.list.filters.recordIdentifier.item.holdingsUUID',
     },
   ],
+  HOLDINGS_RECORD: [
+    {
+      value: '',
+      label: 'ui-bulk-edit.list.filters.recordIdentifier.placeholder',
+    },
+    {
+      value: 'ID',
+      label: 'ui-bulk-edit.list.filters.recordIdentifier.holdings.holdingsUUID',
+    },
+    {
+      value: 'HOLDINGS_RECORD_ID',
+      label: 'ui-bulk-edit.list.filters.recordIdentifier.holdings.holdingsHRID',
+    },
+    {
+      value: 'INSTANCE_HRID',
+      label: 'ui-bulk-edit.list.filters.recordIdentifier.holdings.instanceHRID',
+    },
+    {
+      value: 'ITEM_BARCODE',
+      label: 'ui-bulk-edit.list.filters.recordIdentifier.holdings.itemBarcodes',
+    },
+  ],
 };
 
 export const EDIT_CAPABILITIES = [
@@ -65,6 +87,11 @@ export const EDIT_CAPABILITIES = [
   {
     value: 'ITEMS',
     label: <FormattedMessage id="ui-bulk-edit.list.filters.capabilities.inventory" />,
+    disabled: false,
+  },
+  {
+    value: 'HOLDINGS_RECORD',
+    label: <FormattedMessage id="ui-bulk-edit.list.filters.capabilities.holdings" />,
     disabled: false,
   },
   // Commented in scope of story UIBULKED-68//
