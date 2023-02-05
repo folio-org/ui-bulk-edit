a13f68e9c5fda45e40e3ed270e0e2f0d74e81f5d
Mikita Siadykh <mikita_siadykh@epam.com>
2023-02-05 08:47:07 +0400

    ifx loan type

--- src/hooks/api/useLoanTypes.js
+++ src/hooks/api/useLoanTypes.js
@@ -15,7 +15,7 @@ export const useLoanTypes = () => {
 
   if (data) {
     return {
-      loanTypes: data.loantypes.map(type => ({ label: type.name, value: type.name })),
+      loanTypes: data.loantypes.map(type => ({ label: type.name, value: type.id })),
       isLoading,
     };
   }
