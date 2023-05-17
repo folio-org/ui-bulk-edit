d3a1d4343fa022186990d322873be8ce25883d21
Uladzislau_Kutarkin <uladzislau_kutarkin@epam.com>
2023-05-18 00:40:52 +0400

    UIBULKED-292: fix comments

--- src/hooks/useLocationSorting.js
+++ src/hooks/useLocationSorting.js
@@ -1,6 +1,12 @@
+import {
+  useCallback,
+  useEffect,
+} from 'react';
+import {
+  buildSearch,
+  buildSortingObj,
+} from '@folio/stripes-acq-components';
 import useSorting from '@folio/stripes-acq-components/lib/AcqList/hooks/useSorting';
-import { useCallback, useEffect } from 'react';
-import { buildSearch, buildSortingObj } from '@folio/stripes-acq-components/lib/AcqList/utils';
 
 export const useLocationSorting = (location, history, resetData, sortableFields, defaultSorting) => {
   const [
@@ -18,7 +24,6 @@ export const useLocationSorting = (location, history, resetData, sortableFields,
       setSortingField(initialSorting.sortingField);
       setSortingDirection(initialSorting.sortingDirection);
     },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     [resetData, location.search],
   );
 
