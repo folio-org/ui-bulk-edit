cde00e8a677a8cb1e0f3ff297a01d85d0418ae63
Uladzislau_Kutarkin <uladzislau_kutarkin@epam.com>
2023-06-16 12:03:05 +0400

    UIBULKED-310: Apply to all items checkbox - default values

--- src/components/BulkEditList/BulkEditListResult/BulkEditInApp/ContentUpdatesForm/AdditionalActions.js
+++ src/components/BulkEditList/BulkEditListResult/BulkEditInApp/ContentUpdatesForm/AdditionalActions.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Checkbox, Col } from '@folio/stripes/components';
 import PropTypes from 'prop-types';
 import { useIntl } from 'react-intl';
 import { useLocation } from 'react-router-dom';
-import { ACTIONS, CAPABILITIES, CONTROL_TYPES } from '../../../../../constants';
+import { CAPABILITIES, CONTROL_TYPES } from '../../../../../constants';
 import { WITH_ITEMS_VALUE_KEY } from './helpers';
+import { useDerivativeModification } from '../../../../../hooks';
 
 export const AdditionalActions = ({ action, actionIndex, onChange }) => {
   const { formatMessage } = useIntl();
@@ -12,15 +13,7 @@ export const AdditionalActions = ({ action, actionIndex, onChange }) => {
   const search = new URLSearchParams(location.search);
   const capability = search.get('capabilities');
 
-  useEffect(() => {
-    if (action.name === ACTIONS.SET_TO_TRUE) {
-      onChange({ actionIndex, value: true, fieldName: WITH_ITEMS_VALUE_KEY });
-    }
-
-    if (action.name === ACTIONS.SET_TO_FALSE) {
-      onChange({ actionIndex, value: false, fieldName: WITH_ITEMS_VALUE_KEY });
-    }
-  }, [action.name]);
+  useDerivativeModification({ onChange, actionIndex, action: action.name, deps: [action.name] });
 
   const renderSuppressCheckbox = () => {
     return capability === CAPABILITIES.HOLDING && action.type === CONTROL_TYPES.SUPPRESS_CHECKBOX && (
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,3 +2,4 @@ export * from './usePathParams';
 export * from './useLocationFilters';
 export * from './useBulkPermissions';
 export * from './useLogsQueryParams';
+export * from './useDerivativeModification';
