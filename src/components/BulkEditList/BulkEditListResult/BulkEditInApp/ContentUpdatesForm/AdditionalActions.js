6ae8a54325737bb34ffcd565109b0ffaa9ff351c
Uladzislau_Kutarkin <uladzislau_kutarkin@epam.com>
2023-06-16 11:39:02 +0400

    UIBULKED-310: Apply to all items checkbox - default values

--- src/components/BulkEditList/BulkEditListResult/BulkEditInApp/ContentUpdatesForm/AdditionalActions.js
+++ src/components/BulkEditList/BulkEditListResult/BulkEditInApp/ContentUpdatesForm/AdditionalActions.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Checkbox, Col } from '@folio/stripes/components';
 import PropTypes from 'prop-types';
 import { useIntl } from 'react-intl';
 import { useLocation } from 'react-router-dom';
-import { CAPABILITIES, CONTROL_TYPES } from '../../../../../constants';
+import { ACTIONS, CAPABILITIES, CONTROL_TYPES } from '../../../../../constants';
 import { WITH_ITEMS_VALUE_KEY } from './helpers';
 
 export const AdditionalActions = ({ action, actionIndex, onChange }) => {
@@ -12,12 +12,23 @@ export const AdditionalActions = ({ action, actionIndex, onChange }) => {
   const search = new URLSearchParams(location.search);
   const capability = search.get('capabilities');
 
+  useEffect(() => {
+    if (action.name === ACTIONS.SET_TO_TRUE) {
+      onChange({ actionIndex, value: true, fieldName: WITH_ITEMS_VALUE_KEY });
+    }
+
+    if (action.name === ACTIONS.SET_TO_FALSE) {
+      onChange({ actionIndex, value: false, fieldName: WITH_ITEMS_VALUE_KEY });
+    }
+  }, [action.name]);
+
   const renderSuppressCheckbox = () => {
     return capability === CAPABILITIES.HOLDING && action.type === CONTROL_TYPES.SUPPRESS_CHECKBOX && (
       <Col xs={2} sm={2}>
         <Checkbox
           label={formatMessage({ id: 'ui-bulk-edit.layer.action.applyItems' })}
           value={action.value}
+          checked={action.withItems}
           onChange={e => onChange({ actionIndex, value: e.target.checked, fieldName: WITH_ITEMS_VALUE_KEY })}
         />
       </Col>
@@ -37,6 +48,7 @@ AdditionalActions.propTypes = {
     type: PropTypes.string,
     name: PropTypes.string,
     value: PropTypes.string,
+    withItems: Promise.bool,
   }),
   actionIndex: PropTypes.number,
   onChange: PropTypes.func,
