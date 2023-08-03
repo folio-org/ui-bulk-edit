6052a63334639d0a97fe6a43130dbd85f2891bd8
Uladzislau_Kutarkin <uladzislau_kutarkin@epam.com>
2023-08-03 10:27:43 +0400

    UIBULKED-321: fix test fails

--- src/hooks/useBulkPermissions.js
+++ src/hooks/useBulkPermissions.js
@@ -75,7 +75,6 @@ export const useBulkPermissions = () => {
     hasViewInAppPerms,
     hasItemInventoryEdit,
     hasHoldingsInventoryEdit,
-    hasItemsAndHoldingsInventoryEdit,
     hasItemInventoryView,
     hasHoldingsInventoryView,
     hasItemsAndHoldingsInventoryView,
