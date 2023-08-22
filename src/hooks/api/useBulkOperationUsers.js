699b5a83d10d191d48e2fec901a8fc2095354c7e
vashjs <vadym.shchekotilin@gmail.com>
2023-08-22 14:31:28 +0200

    UIBULKED-318 Add missed permission and update query for User filter on Logs tab

--- src/hooks/api/useBulkOperationUsers.js
+++ src/hooks/api/useBulkOperationUsers.js
@@ -1,12 +1,13 @@
 import { useQuery } from 'react-query';
 import { useOkapiKy } from '@folio/stripes/core';
+import { CAPABILITIES } from '../../constants';
 
 export const useBulkOperationUsers = (options) => {
   const ky = useOkapiKy();
 
   const { data, isLoading } = useQuery({
     queryKey: ['bulkOperationUsers'],
-    queryFn: () => ky.get('bulk-operations/list-users?query=(entityType=="USER")&limit=100&offset=0').json(),
+    queryFn: () => ky.get(`bulk-operations/list-users?query=(entityType==("${CAPABILITIES.USER}" or "${CAPABILITIES.ITEM}" or "${CAPABILITIES.HOLDING}"))&limit=100&offset=0`).json(),
     ...options,
   });
 
