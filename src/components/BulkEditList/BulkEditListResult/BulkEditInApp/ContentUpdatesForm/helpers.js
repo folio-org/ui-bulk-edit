11692cb9a98df6d0e0156b7fcd9c639595b8f2e7
Uladzislau_Kutarkin <uladzislau_kutarkin@epam.com>
2023-10-25 00:05:37 +0400

    UIBULKED-316: refactor code

--- src/components/BulkEditList/BulkEditListResult/BulkEditInApp/ContentUpdatesForm/helpers.js
+++ src/components/BulkEditList/BulkEditListResult/BulkEditInApp/ContentUpdatesForm/helpers.js
@@ -272,25 +272,8 @@ export const getDefaultActions = (option, options, formatMessage) => {
           },
         ],
       };
-    case OPTIONS.ITEM_NOTE:
-      return {
-        type: '',
-        actions: [
-          null,
-          {
-            actionsList: noteWithMarkDefaultActions,
-            controlType: (action) => {
-              return action === ACTIONS.CHANGE_TYPE
-                ? CONTROL_TYPES.NOTE_SELECT
-                : CONTROL_TYPES.TEXTAREA;
-            },
-            [ACTION_VALUE_KEY]: noteWithMarkDefaultActions[0].value,
-            [FIELD_VALUE_KEY]: '',
-          },
-        ],
-      };
-
     case OPTIONS.HOLDINGS_NOTE:
+    case OPTIONS.ITEM_NOTE:
       return {
         type: '',
         actions: [
