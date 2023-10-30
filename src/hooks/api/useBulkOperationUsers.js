c3e8453d02d975fb1c38e00eec4e9a0c0801a040
vashjs <86330150+vashjs@users.noreply.github.com>
2023-10-30 21:50:30 +0000

    UIBULKED-373 "User" dropdown doesn't include Users recently run bulk edit jobs (#403)

--- src/hooks/api/useBulkOperationUsers.js
+++ src/hooks/api/useBulkOperationUsers.js
@@ -7,7 +7,7 @@ export const useBulkOperationUsers = (options) => {
 
   const { data, isLoading } = useQuery({
     queryKey: ['bulkOperationUsers'],
-    queryFn: () => ky.get(`bulk-operations/list-users?query=(entityType==("${CAPABILITIES.USER}" or "${CAPABILITIES.ITEM}" or "${CAPABILITIES.HOLDING}"))&limit=100&offset=0`).json(),
+    queryFn: () => ky.get(`bulk-operations/list-users?query=(entityType==("${CAPABILITIES.USER}" or "${CAPABILITIES.ITEM}" or "${CAPABILITIES.HOLDING}"))&offset=0`).json(),
     ...options,
   });
 
