e3525949c6a13a9c72171df4aee7f09a101c8eef
Uladzislau_Kutarkin <uladzislau_kutarkin@epam.com>
2023-11-21 15:37:13 +0400

    UIBULKED-359: Update Electronic access - Materials specified

--- src/components/BulkEditList/BulkEditListResult/BulkEditInApp/ContentUpdatesForm/helpers.test.js
+++ src/components/BulkEditList/BulkEditListResult/BulkEditInApp/ContentUpdatesForm/helpers.test.js
@@ -692,6 +692,44 @@ describe('ContentUpdatesForm helpers', () => {
           );
       });
 
+      it('returns the correct object for the ELECTRONIC_ACCESS_MATERIALS_SPECIFIED option', () => {
+        expect(JSON.stringify(getDefaultActions(OPTIONS.ELECTRONIC_ACCESS_MATERIALS_SPECIFIED, [], formatMessage)))
+          .toEqual(
+            JSON.stringify({
+              type: '',
+              actions: [
+                null,
+                {
+                  actionsList: [{
+                    value: '',
+                    disabled: true,
+                    label: undefined,
+                  },
+                  {
+                    value: ACTIONS.CLEAR_FIELD,
+                    disabled: false,
+                    label: undefined
+                  },
+                  {
+                    value: ACTIONS.FIND,
+                    disabled: false,
+                    label: undefined
+                  },
+                  {
+                    value: ACTIONS.REPLACE_WITH,
+                    disabled: false,
+                    label: undefined
+                  },
+                  ],
+                  controlType: () => CONTROL_TYPES.TEXTAREA,
+                  [ACTION_VALUE_KEY]: '',
+                  [FIELD_VALUE_KEY]: '',
+                },
+              ],
+            }),
+          );
+      });
+
       it('returns the correct object for the default case', () => {
         expect(getDefaultActions('unknown', [], formatMessage))
           .toEqual({
