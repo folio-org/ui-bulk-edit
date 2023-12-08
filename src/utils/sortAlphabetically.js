dc8cdde625a2f8179032a11672a3c7d2c97b4716
UladzislauKutarkin <72550466+UladzislauKutarkin@users.noreply.github.com>
2023-12-08 15:50:36 +0400

    UIBULKED-375: Localize alphabetical order of Bulk edit elements (#424)

--- src/utils/sortAlphabetically.js
+++ src/utils/sortAlphabetically.js
@@ -1,11 +1,30 @@
 export const sortAlphabetically = (array, placeholder) => array.sort((a, b) => {
+  const collator = new Intl.Collator();
+
   if (a.label === placeholder) {
     return -1;
-  } else if (placeholder) {
+  } else if (b.label === placeholder) {
     return 1;
-  } else {
-    return a.label.localeCompare(b.label);
   }
+  // Compare based on category
+  const aCategory = a.categoryName || '';
+  const bCategory = b.categoryName || '';
+  const categoryComparison = collator.compare(aCategory, bCategory);
+
+  // If one item has no category, compare label to category name
+  if (!a.categoryName && b.categoryName) {
+    return collator.compare(a.label, bCategory);
+  } else if (a.categoryName && !b.categoryName) {
+    return collator.compare(aCategory, b.label);
+  }
+
+  // If same category, sort based on label with lower priority
+  if (categoryComparison === 0) {
+    return collator.compare(a.label, b.label);
+  }
+
+  // Prioritize sorting based on category
+  return categoryComparison;
 });
 
 export const sortAlphabeticallyActions = (array, placeholder) => {
