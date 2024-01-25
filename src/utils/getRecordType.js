620ecee1917863e7aa9c32f28ef9b8bd3b607ccb
Uladzislau_Kutarkin <uladzislau_kutarkin@epam.com>
2024-01-25 11:55:07 +0400

    UIBULKED-403: replace old method

--- src/utils/getRecordType.js
+++ src/utils/getRecordType.js
@@ -1,7 +1,7 @@
 import { CAPABILITIES, RECORD_TYPES } from '../constants';
 
 export const getRecordType = (capability) => {
-  if (Object.prototype.hasOwnProperty.call(CAPABILITIES, capability)) {
+  if (Object.hasOwn(CAPABILITIES, capability)) {
     return RECORD_TYPES[CAPABILITIES[capability]];
   } else {
     return null;
