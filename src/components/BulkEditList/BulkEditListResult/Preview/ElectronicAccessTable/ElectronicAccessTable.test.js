ea02c1f40211939fc9f5db21b6727aadda609e19
vashjs <vadym.shchekotilin@gmail.com>
2024-02-07 10:47:54 +0100

    UIBULKED-384 Change delimiters to split string

--- src/components/BulkEditList/BulkEditListResult/Preview/ElectronicAccessTable/ElectronicAccessTable.test.js
+++ src/components/BulkEditList/BulkEditListResult/Preview/ElectronicAccessTable/ElectronicAccessTable.test.js
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { ElectronicAccessTable } from './ElectronicAccessTable';
 
 
-const testValue = 'https://search.proquest.com/publication/1396348;test;1.2012 -;via ProQuest, the last 12 months are not available due to an embargo;Resource';
+const testValue = 'https://search.proquest.com/publication/1396348\u001f;test\u001f;1.2012 -\u001f;via ProQuest, the last 12 months are not available due to an embargo\u001f;Resource';
 describe('ElectronicAccessTable', () => {
   it('renders table headers correctly', () => {
     const { getByText } = render(<ElectronicAccessTable value={testValue} />);
@@ -21,7 +21,7 @@ describe('ElectronicAccessTable', () => {
 
   it('renders table body rows correctly', () => {
     const { getByText } = render(<ElectronicAccessTable value={testValue} />);
-    const tableBodyRows = testValue.split('|').map(row => row.split(';'));
+    const tableBodyRows = testValue.split('\u001f|').map(row => row.split('\u001f;'));
 
     tableBodyRows.forEach(row => {
       row.forEach(cell => {
