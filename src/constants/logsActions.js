9e948b1858bc396d83d899cc40724d2a083fbcc7
vashjs <vadym.shchekotilin@gmail.com>
2024-08-13 23:27:21 +0200

    update test

--- src/constants/logsActions.js
+++ src/constants/logsActions.js
@@ -3,6 +3,7 @@ export const linkNamesMap = {
   linkToMatchedRecordsCsvFile: 'MATCHED_RECORDS_FILE',
   linkToMatchedRecordsErrorsCsvFile: 'RECORD_MATCHING_ERROR_FILE',
   linkToModifiedRecordsCsvFile: 'PROPOSED_CHANGES_FILE',
+  linkToModifiedRecordsMarcFile: 'PROPOSED_CHANGES_LINK_MARC',
   linkToCommittedRecordsCsvFile: 'COMMITTED_RECORDS_FILE',
   linkToCommittedRecordsErrorsCsvFile: 'COMMITTING_CHANGES_ERROR_FILE',
   expired: 'expired',
