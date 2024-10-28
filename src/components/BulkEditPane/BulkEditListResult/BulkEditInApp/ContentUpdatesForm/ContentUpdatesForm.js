f661fc5cfb48db6cd7de17d7df69d8cd2e7cf8f8
UladzislauKutarkin <72550466+UladzislauKutarkin@users.noreply.github.com>
2024-10-28 11:40:34 +0400

    UIBULKED-497: "Are you sure" preview displays outdated values after User changed selection on bulk edit form and clicked "Confirm changes" (#630)

--- src/components/BulkEditPane/BulkEditListResult/BulkEditInApp/ContentUpdatesForm/ContentUpdatesForm.js
+++ src/components/BulkEditPane/BulkEditListResult/BulkEditInApp/ContentUpdatesForm/ContentUpdatesForm.js
@@ -237,7 +237,7 @@ export const ContentUpdatesForm = ({
 
         return {
           option: mappedOption,
-          tenants,
+          tenants: tenants?.filter(Boolean),
           actions: [{
             type,
             initial,
