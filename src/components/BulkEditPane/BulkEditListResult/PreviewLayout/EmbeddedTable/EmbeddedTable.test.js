a17c7a8c0d483080464bf38dbb62bc20458f3bf5
vashjs <vadym.shchekotilin@gmail.com>
2025-04-03 09:20:17 +0200

    update tests

---
+++ src/components/BulkEditPane/BulkEditListResult/PreviewLayout/EmbeddedTable/EmbeddedTable.test.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import { runAxeTest } from '@folio/stripes-testing';
+
+import { EmbeddedTable } from './EmbeddedTable';
+import { ELECTRONIC_ACCESS_HEAD_TITLES, SUBJECT_HEAD_TITLES } from '../../../../PermissionsModal/constants/lists';
+
+
+const electronicAccessValue = 'https://search.proquest.com/publication/1396348\u001f;test\u001f;1.2012 -\u001f;via ProQuest, the last 12 months are not available due to an embargo\u001f;Resource';
+const subjectValue = 'head\u001f;source\u001f;title';
+
+
+describe('EmbeddedTable', () => {
+  describe('Electronic access table', () => {
+    it('renders table headers correctly', () => {
+      const { getByText } = render(<EmbeddedTable value={electronicAccessValue} headTitles={ELECTRONIC_ACCESS_HEAD_TITLES} />);
+      [
+        'ui-bulk-edit.list.preview.electronicAccess.relationship',
+        'ui-bulk-edit.list.preview.electronicAccess.uri',
+        'ui-bulk-edit.list.preview.electronicAccess.linkText',
+        'ui-bulk-edit.list.preview.electronicAccess.materialsSpecified',
+        'ui-bulk-edit.list.preview.electronicAccess.publicNote',
+      ].forEach(header => {
+        expect(getByText(header))
+          .toBeInTheDocument();
+      });
+    });
+
+    it('renders table body rows correctly', () => {
+      const { getByText } = render(<EmbeddedTable value={electronicAccessValue} headTitles={ELECTRONIC_ACCESS_HEAD_TITLES} />);
+      const tableBodyRows = electronicAccessValue.split('\u001f|')
+        .map(row => row.split('\u001f;'));
+
+      tableBodyRows.forEach(row => {
+        row.forEach(cell => {
+          expect(getByText(cell))
+            .toBeInTheDocument();
+        });
+      });
+    });
+  });
+
+  describe('Subject table', () => {
+    it('renders table headers correctly', () => {
+      const { getByText } = render(<EmbeddedTable value={subjectValue} headTitles={SUBJECT_HEAD_TITLES} />);
+      [
+        'ui-bulk-edit.list.preview.subject.subjectHeadings',
+        'ui-bulk-edit.list.preview.subject.subjectSource',
+        'ui-bulk-edit.list.preview.subject.subjectType',
+      ].forEach(header => {
+        expect(getByText(header))
+          .toBeInTheDocument();
+      });
+    });
+
+    it('renders table body rows correctly', () => {
+      const { getByText } = render(<EmbeddedTable value={subjectValue} headTitles={SUBJECT_HEAD_TITLES} />);
+      const tableBodyRows = subjectValue.split('\u001f|')
+        .map(row => row.split('\u001f;'));
+
+      tableBodyRows.forEach(row => {
+        row.forEach(cell => {
+          expect(getByText(cell))
+            .toBeInTheDocument();
+        });
+      });
+    });
+  });
+
+  it('should render with no axe errors', async () => {
+    render(<EmbeddedTable value={electronicAccessValue} headTitles={ELECTRONIC_ACCESS_HEAD_TITLES} />);
+
+    await runAxeTest({
+      rootNode: document.body,
+    });
+  });
+});
