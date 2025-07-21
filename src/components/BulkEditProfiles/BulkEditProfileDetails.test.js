d7d05943fae0deef5736b5ef29e2114ea3ab9b74
Yury Saukou <yury_saukou@epam.com>
2025-07-21 12:24:51 +0200

    implement base test

---
+++ src/components/BulkEditProfiles/BulkEditProfileDetails.test.js
@@ -0,0 +1,65 @@
+import {
+  render,
+  screen,
+} from '@folio/jest-config-stripes/testing-library/react';
+import { runAxeTest } from '@folio/stripes-testing';
+
+import { CAPABILITIES } from '../../constants';
+import { useBulkEditProfile } from '../../hooks/api';
+import { BulkEditProfileDetails } from './BulkEditProfileDetails';
+
+jest.mock('@folio/stripes/smart-components', () => ({
+  ...jest.requireActual('@folio/stripes/smart-components'),
+  ViewMetaData: jest.fn(() => <div>ViewMetaData</div>),
+}));
+
+jest.mock('@folio/stripes-acq-components', () => ({
+  ...jest.requireActual('@folio/stripes-acq-components'),
+  handleKeyCommand: jest.fn((cb) => { cb?.(); }),
+}));
+
+jest.mock('../../hooks/api', () => ({
+  useBulkEditProfile: jest.fn(),
+}));
+
+const profileMock = {
+  id: '123',
+  name: 'Test Profile',
+  description: 'This is a test profile',
+};
+
+const defaultProps = {
+  entityType: CAPABILITIES.USER,
+  match: { params: { id: '123' } },
+  onClose: jest.fn(),
+};
+
+const renderBulkEditProfileDetails = (props = {}) => render(
+  <BulkEditProfileDetails
+    {...defaultProps}
+    {...props}
+  />,
+);
+
+describe('BulkEditProfileDetails', () => {
+  beforeEach(() => {
+    useBulkEditProfile.mockReturnValue({ profile: profileMock });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render profiles details', () => {
+    renderBulkEditProfileDetails();
+
+    expect(screen.getByText('ui-bulk-edit.settings.profiles.columns.name')).toBeInTheDocument();
+    expect(screen.getByText('ui-bulk-edit.settings.profiles.columns.description')).toBeInTheDocument();
+  });
+
+  it('should have proper accessibility attributes', async () => {
+    const { container } = renderBulkEditProfileDetails();
+
+    await runAxeTest({ rootNode: container });
+  });
+});
