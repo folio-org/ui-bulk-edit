5423b7610a360a8aba0c406760c2eceb6a57f501
Yury Saukou <yury_saukou@epam.com>
2025-08-05 19:04:31 +0400

    tests

---
+++ src/components/BulkEditProfiles/BulkEditProfileBulkEditsDetails.test.js
@@ -0,0 +1,85 @@
+import {
+  render,
+  screen,
+} from '@folio/jest-config-stripes/testing-library/react';
+import { runAxeTest } from '@folio/stripes-testing';
+
+import {
+  CAPABILITIES,
+  getUserOptions,
+} from '../../constants';
+import { useOptionsWithTenants } from '../../hooks/useOptionsWithTenants';
+import { BulkEditProfileBulkEditsDetails } from './BulkEditProfileBulkEditsDetails';
+
+jest.mock('@folio/stripes/components', () => ({
+  ...jest.requireActual('@folio/stripes/components'),
+  Loading: jest.fn(() => <div>Loading</div>),
+}));
+
+jest.mock('../../hooks/useOptionsWithTenants', () => ({
+  useOptionsWithTenants: jest.fn(),
+}));
+
+const values = [
+  {
+    id: '123',
+    option: 'ADMINISTRATIVE_NOTE',
+    tenants: [],
+    actionsDetails: {
+      actions: [
+        {
+          name: 'ADD_TO_EXISTING',
+          value: 'foo',
+          parameters: [],
+          tenants: []
+        }
+      ]
+    }
+  },
+];
+
+const defaultProps = {
+  entityType: CAPABILITIES.USER,
+  isLoading: false,
+  values,
+};
+
+const renderBulkEditProfileBulkEditsDetails = (props = {}) => render(
+  <BulkEditProfileBulkEditsDetails
+    {...defaultProps}
+    {...props}
+  />,
+);
+
+describe('BulkEditProfileBulkEditsDetails', () => {
+  beforeEach(() => {
+    useOptionsWithTenants.mockReturnValue({
+      areAllOptionsLoaded: true,
+      options: getUserOptions(jest.fn()),
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render loading state when isLoading is true', () => {
+    renderBulkEditProfileBulkEditsDetails({ isLoading: true });
+
+    expect(screen.getByText('Loading')).toBeInTheDocument();
+  });
+
+  it('should render profile bulk edits', () => {
+    renderBulkEditProfileBulkEditsDetails();
+
+    expect(screen.getByText('ui-bulk-edit.layer.column.options')).toBeInTheDocument();
+    expect(screen.getByText('ui-bulk-edit.layer.column.actions')).toBeInTheDocument();
+    expect(screen.getByText('ui-bulk-edit.layer.column.data')).toBeInTheDocument();
+  });
+
+  it('should have proper accessibility attributes', async () => {
+    const { container } = renderBulkEditProfileBulkEditsDetails();
+
+    await runAxeTest({ rootNode: container });
+  });
+});
