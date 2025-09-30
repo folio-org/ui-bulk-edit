9fe2e28b970d1e13266c5e3c55577249056e5e44
vashjs <vadym.shchekotilin@gmail.com>
2025-09-30 10:04:53 +0200

    update comment

--- src/components/BulkEditPane/BulkEditListResult/PreviewLayout/EmbeddedTable/EmbeddedTable.js
+++ src/components/BulkEditPane/BulkEditListResult/PreviewLayout/EmbeddedTable/EmbeddedTable.js
@@ -3,6 +3,7 @@ import { DynamicTable } from '@folio/plugin-query-builder';
 
 export const EmbeddedTable = ({ value, headTitles }) => {
   // Split rows by unit separator and cells by record separator + map to required format for DynamicTable
+  // \u001f and \u001e are non-printable characters used as delimiters
   const values = value?.split('\u001f|')
     .map(row => {
       return row.split('\u001f;')
