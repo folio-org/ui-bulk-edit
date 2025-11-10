ff88d1aac71570b2f339a4a006b2d352321fb35b
vashjs <vadym_shchekotilin@epam.com>
2025-11-10 01:13:26 +0100

    update

--- src/components/BulkEditProfiles/BulkEditProfiles.js
+++ src/components/BulkEditProfiles/BulkEditProfiles.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 import { FormattedMessage } from 'react-intl';
 import {
   FormattedDate,
