be2f5319c33c9b6a113e60d474d7010f6943334b
Vadym S <86330150+vashjs@users.noreply.github.com>
2026-07-01 00:24:15 +0200

    UIBULKED-99 ROLLLBACK Treat delete permission as implying view (#854)

--- src/hooks/useBulkPermissions.js
+++ src/hooks/useBulkPermissions.js
@@ -28,12 +28,9 @@ export const useBulkPermissions = () => {
   const hasQuickMarcViewPerms = stripes.hasPerm('ui-quick-marc.quick-marc-editor.view');
 
   // Users
-  // `ui-users.delete` lives in the separate `ui-users` module, so we can't make it
-  // include `ui-users.view` as sub-permissions here. Instead we treat
-  // delete as implying view, so granting delete grants view under the hood.
-  const hasUsersDeletePerms = stripes.hasPerm('ui-users.delete');
   const hasUsersPerms = stripes.hasPerm('ui-users.edit');
-  const hasUsersViewPerms = stripes.hasPerm('ui-users.view') || hasUsersDeletePerms;
+  const hasUsersViewPerms = stripes.hasPerm('ui-users.view');
+  const hasUsersDeletePerms = stripes.hasPerm('ui-users.delete');
 
   // derived pages
   const hasAnyInAppEditPermissions = hasInAppEditPerms || hasInAppUsersEditPerms;
